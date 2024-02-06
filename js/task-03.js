d19cd69b3cca5dc80300c17ada69aee4c7b3c478
RoksanaKor <roksanakorban@gmail.com>
2024-02-07 00:32:59 +0100

    task 3 no grid

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector(".gallery");
+
+
+const outputImages = images
+.map((image) => `<li><img src="${image.url} alt="${image.alt}"/></li>`)
+.join("");
+
+console.log(outputImages);
+
+gallery.insertAdjacentHTML("afterbegin", outputImages);
