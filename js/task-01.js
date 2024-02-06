64d19e6cd86578236e2f9d66003fc2d4b3b057eb
RoksanaKor <roksanakorban@gmail.com>
2024-02-06 18:49:54 +0100

    first commit

---
+++ js/task-01.js
@@ -0,0 +1,20 @@
+const categories = document.getElementById("categories")
+let categoriesNumber = categories.childElementCount;
+console.log(`Number of categories: ${categoriesNumber}`);
+console.log("");
+
+
+let list = document.querySelectorAll(".item");
+let arrayItems = [...list]
+let headings = document.getElementsByTagName("h2");
+let listElements = document.querySelectorAll('.item ul');
+
+
+function consoleElements(array) {
+ for (let i=0; i<array.length; i++) {
+  console.log(`Category: ${headings[i].innerHTML}`);
+  console.log(`Elements: ${listElements[i].childElementCount}`);
+  console.log("");
+ }
+}
+consoleElements(arrayItems)
