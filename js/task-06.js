34afe661cec1f28ea8df7655826c3e6e447a122b
RoksanaKor <roksanakorban@gmail.com>
2024-02-08 17:18:16 +0100

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputToValidate = document.getElementById("validation-input");
+
+const numberValue = Number(inputToValidate.dataset.length);
+
+
+inputToValidate.addEventListener("input", () => {
+ if (inputToValidate.value.length === numberValue) {
+  inputToValidate.classList.replace("invalid","valid");
+ } else {
+  inputToValidate.classList.add("invalid");
+ }
+});
+
+
