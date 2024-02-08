049c10b0a9102f1f47461cac092262673ad756f0
RoksanaKor <roksanakorban@gmail.com>
2024-02-08 18:40:30 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const currentBodyColor = document.querySelector(".color");
+
+
+changeColorBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  currentBodyColor.textContent = getRandomHexColor();
+})
