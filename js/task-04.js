b2e8cbbf00a9e78a30e3e46fc2b07b5d7dc8f0bc
RoksanaKor <roksanakorban@gmail.com>
2024-02-08 16:14:25 +0100

    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+const counterValue = document.getElementById("value");
+counterValue.innerHTML = 0;
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+decrementBtn.addEventListener("click", () => {
+ counterValue.innerHTML--;
+ console.log(counterValue.innerHTML);
+})
+
+incrementBtn.addEventListener("click", () => {
+ counterValue.innerHTML++;
+ console.log(counterValue.innerHTML);
+})
