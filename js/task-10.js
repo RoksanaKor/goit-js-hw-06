d13e042319c09849f84432b732e40c825a858270
RoksanaKor <roksanakorban@gmail.com>
2024-02-08 20:13:59 +0100

    finished

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputDiv = document.querySelector("input[type='number']");
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.getElementById("boxes");
+
+function createBoxes() {
+  let width = 30;
+  let height = 30;
+  let boxesArray = [];
+  if (1 <= Number(inputDiv.value) <= 100) {
+    for(let i=0; i<Number(inputDiv.value); i++) {
+      const newBox = document.createElement("div");
+      newBox.style.width = `${width}px`;
+      newBox.style.height = `${height}px`;
+      newBox.style.backgroundColor = getRandomHexColor();
+      boxesArray.push(newBox);
+      width += 10;
+      height += 10;
+    }
+  }
+  boxes.append(...boxesArray);
+}
+
+function destroyBoxes() {
+  while (boxes.firstChild) {
+    boxes.removeChild(boxes.firstChild);
+  }
+}
+
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
