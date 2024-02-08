f2e23ee75de182e8bc1098483364c7caff8ef369
RoksanaKor <roksanakorban@gmail.com>
2024-02-08 18:12:01 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formToSubmit = document.querySelector(".login-form");
 
+formToSubmit.addEventListener("submit", handleSubmit); 
+
+
+function handleSubmit(event) {
+ event.preventDefault();
+ const myForm = event.target;
+ const myEmail = myForm.elements.email.value;
+ const myPassword = myForm.elements.password.value;
+ const userData = {
+  Email: myEmail,
+  Password: myPassword
+ }
+ if (myEmail === "" || myPassword === "") {
+  alert("It is necessary to complete all the fields!");
+ } else {
+  console.log(userData);
+  myForm.reset();
+ }
+}
